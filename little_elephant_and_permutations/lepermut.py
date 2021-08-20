d20f2caf608581000a969a4fe221d6068c9be287
thelongcommute <screeflyer@gmail.com>
2021-08-20 12:21:49 -0700

    Lepermut problem completed

---
+++ little_elephant_and_permutations/lepermut.py
@@ -0,0 +1,11 @@
+T = int(input())
+for _ in range(T):
+    size = int(input())
+    A = [int(x) for x in input().split(" ")]
+    good = "YES"
+    for i in range(size):
+        for j in range(i + 1, size):
+            if (A[i] > A[j]) and (j - i) != 1:  # if there is an inversion and the inversion is not local
+                good = "NO"
+                break
+    print(good)
